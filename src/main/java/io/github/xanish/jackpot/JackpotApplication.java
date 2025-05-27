9d1794259cc8f180f6de864d2ac780ba7d273fb2
xanish <danishali7866@gmail.com>
2025-05-27 21:41:27 +0530

    feat: initialize spring project with dependencies
    - Spring Web (for REST APIs)
    - Spring Kafka (for Kafka integration)
    - Spring Data JPA (for database interaction)
    - H2 Database (for in-memory persistence)
    - Lombok (to reduce boilerplate code)

---
+++ src/main/java/io/github/xanish/jackpot/JackpotApplication.java
@@ -0,0 +1,13 @@
+package io.github.xanish.jackpot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JackpotApplication.class, args);
+	}
+
+}
