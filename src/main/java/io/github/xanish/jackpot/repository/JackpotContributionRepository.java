d08a49d16f4b16844734e20077057f33019495b6
xanish <danishali7866@gmail.com>
2025-05-29 13:58:42 +0530

    fix: missing findByBetId method
    - Add method signature to allow jpa to fetch contribution by bet id

--- src/main/java/io/github/xanish/jackpot/repository/JackpotContributionRepository.java
+++ src/main/java/io/github/xanish/jackpot/repository/JackpotContributionRepository.java
@@ -1,9 +1,12 @@
 package io.github.xanish.jackpot.repository;
 
 import io.github.xanish.jackpot.model.JackpotContribution;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JackpotContributionRepository
-    extends JpaRepository<JackpotContribution, Long> {}
+    extends JpaRepository<JackpotContribution, Long> {
+    Optional<JackpotContribution> findByBetId(String betId);
+}
